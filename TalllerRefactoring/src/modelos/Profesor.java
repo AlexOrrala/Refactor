a6bfddea27f4401ecae81d62dc6289dbaf3c02cb
Alexorrala <ajorrala@espol.edu.ec>
2022-12-22 22:23:49 -0500

    Primer avaance

---
+++ TalllerRefactoring/src/modelos/Profesor.java
@@ -0,0 +1,38 @@
+package modelos;
+
+import java.util.ArrayList;
+
+public class Profesor {
+    public String codigo;
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String direccion;
+    public String telefono;
+    public int añosdeTrabajo;
+    public String facultad;
+    public double BonoFijo;
+    public ArrayList<Paralelo> paralelos;
+
+    public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        paralelos= new ArrayList<>();
+    }
+    
+    public void anadirParalelos(Paralelo p){
+        paralelos.add(p);
+    }
+    
+    public double calcularSueldo(Profesor prof){
+        double sueldo=0;
+        sueldo= añosdeTrabajo*600 + BonoFijo;
+        return sueldo;
+    }  
+    
+    
+}
